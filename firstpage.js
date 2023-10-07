8e6bd3eb732d317613e10aa31e275759a804b7cf
fathi <fathizidan52@gmail.com>
2023-10-07 18:44:48 +0300

    update

--- firstpage.js
+++ firstpage.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('startGame');
 const input = document.getElementById('winningScore');
 function start() {
     const winningScore = parseInt(input.value);
-    window.location.href = `index.html`;
+    window.location.href = `DiceGame.html`;
     localStorage.setItem('winningScore', winningScore);
 }
 btn.addEventListener("click", start);
